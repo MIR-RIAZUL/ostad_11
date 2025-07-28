fb4a2f67c2ee45208603b4b664f1ca0a29cd2c35
riazul <mirriazul859@gmail.com>
2025-07-28 13:52:45 +0600

    Tab bar and others

--- lib/module_8/stack.dart
+++ lib/module_8/stack.dart
@@ -11,80 +11,94 @@ class module8_stack extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blueGrey,
       ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Stack(
-            children: [
-              Container(height: 200, width: 200, color: Colors.red),
-              Positioned(
-                bottom: 50,
-                left: 10,
-                right: 10,
-                child: Container(height: 200, width: 200, color: Colors.green),
-              ),
-              Positioned(
-                top: 50,
-                left: 20,
-                right: 25,
-                child: Container(height: 200, width: 200, color: Colors.purple),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Stack(
-            children: [
-              CircleAvatar(
-                radius: 100,
-                backgroundImage: NetworkImage(
-                  "https://scontent.fdac189-1.fna.fbcdn.net/v/t39.30808-6/497877028_2403832869991141_8963298409974042492_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeFObH63fsKRHqg2qZlGoxywwxm0Gm_cgDnDGbQab9yAOW_dq4Yh0gP7MkxpuXFazuIASzmEUGrsfEmdCMAGWkSE&_nc_ohc=GowmreEds4YQ7kNvwElOCnE&_nc_oc=AdnGvg9B1EAkX6qivxJN7IoJfPg2rZLAgPfaeAxOlGJouj7nsgCXTE3z92qgfYtYOm8&_nc_zt=23&_nc_ht=scontent.fdac189-1.fna&_nc_gid=axA-Q8wM2KGNpBy9ARSX7Q&oh=00_AfT2kZbYW2UCjRWGq4fpSRkd0TxgboukHflSgarAOSNzWg&oe=6889B825",
-                ),
-              ),
-              Positioned(
-                bottom: 30,
-                right: 30,
-                child: Container(
-                  decoration: BoxDecoration(
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Stack(
+              children: [
+                Container(height: 200, width: 200, color: Colors.red),
+                Positioned(
+                  bottom: 50,
+                  left: 10,
+                  right: 10,
+                  child: Container(
+                    height: 200,
+                    width: 200,
                     color: Colors.green,
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.white54, width: 20),
                   ),
                 ),
-              ),
-            ],
-          ),
-
-          Stack(
-            children: [
-              Container(height: 10, width: double.infinity, color: Colors.red),
-              AnimatedContainer(
-                duration: Duration(seconds: 10),
-                height: 10,
-                width: 200,
-                color: Colors.green,
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Stack(
-            children: [
-              Image.network(
-                "https://i.pinimg.com/736x/c4/d5/88/c4d588819944f1f9043c075f9442ae3d.jpg",
-                height: 300,
-                width: double.infinity,
-              ),
-              Positioned(
-                right: 110,
-                top: 5,
-                child: Text(
-                  "bangladesh",
-                  style: TextStyle(color: Colors.white),
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  right: 25,
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.purple,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+            SizedBox(height: 20),
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 100,
+                  backgroundImage: NetworkImage(
+                    "https://scontent.fdac189-1.fna.fbcdn.net/v/t39.30808-6/497877028_2403832869991141_8963298409974042492_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=86c6b0&_nc_eui2=AeFObH63fsKRHqg2qZlGoxywwxm0Gm_cgDnDGbQab9yAOW_dq4Yh0gP7MkxpuXFazuIASzmEUGrsfEmdCMAGWkSE&_nc_ohc=GowmreEds4YQ7kNvwElOCnE&_nc_oc=AdnGvg9B1EAkX6qivxJN7IoJfPg2rZLAgPfaeAxOlGJouj7nsgCXTE3z92qgfYtYOm8&_nc_zt=23&_nc_ht=scontent.fdac189-1.fna&_nc_gid=axA-Q8wM2KGNpBy9ARSX7Q&oh=00_AfT2kZbYW2UCjRWGq4fpSRkd0TxgboukHflSgarAOSNzWg&oe=6889B825",
+                  ),
+                ),
+                Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white54, width: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            Stack(
+              children: [
+                Container(
+                  height: 10,
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+                AnimatedContainer(
+                  duration: Duration(seconds: 10),
+                  height: 10,
+                  width: 200,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Stack(
+              children: [
+                Image.network(
+                  "https://i.pinimg.com/736x/c4/d5/88/c4d588819944f1f9043c075f9442ae3d.jpg",
+                  height: 300,
+                  width: double.infinity,
+                ),
+                Positioned(
+                  right: 110,
+                  top: 5,
+                  child: Text(
+                    "bangladesh",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/module_9/class_2_module_9.dart
+++ lib/module_9/class_2_module_9.dart
@@ -1,62 +1,130 @@
 import 'package:flutter/material.dart';
 
+import '../module_8/class_3.dart';
+import '../module_8/stack.dart';
+import 'class_1_module_9.dart';
+
 class class2_m9 extends StatelessWidget {
   const class2_m9({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Class 2 Module 9"),
-        backgroundColor: Colors.blueGrey,
-      ),
-      drawer: Drawer(
-        backgroundColor: Colors.white,
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blueGrey),
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius: 45,
-
-                    backgroundImage: NetworkImage(
-                      "https://i.pinimg.com/736x/de/f1/f3/def1f3321c7340363d5e60657d4aef59.jpg",
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "Mir Riazul Islam",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "mirirazul859@gmail.com",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Class 2 Module 9"),
+          backgroundColor: Colors.blueGrey,
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: "home",
+                icon: Icon(Icons.home, color: Colors.white),
               ),
-            ),
-            ListTile(dense: true, title: Text("HOME"), onTap: () {}),
-            Divider(color: Colors.blueGrey, thickness: 1),
-            ListTile(
-              visualDensity: (VisualDensity.compact),
-              title: Text("PROFILE"),
-              onTap: () {},
-            ),
-            Divider(color: Colors.blueGrey, thickness: 1),
-            ListTile(
-              visualDensity: VisualDensity.comfortable,
-              title: Text("SETTINGS"),
-              onTap: () {},
-            ),
-            Divider(color: Colors.blueGrey, thickness: 1),
-            ListTile(
-              visualDensity: VisualDensity(horizontal: 0, vertical: -4),
-              title: Text("LOGOUT"),
-              onTap: () {},
-            ),
-            Divider(color: Colors.blueGrey, thickness: 1),
+              Tab(
+                text: "settings",
+                icon: Icon(Icons.settings, color: Colors.white),
+              ),
+
+              Tab(
+                text: "fav",
+                icon: Icon(Icons.favorite, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.white,
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blueGrey),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 45,
+
+                      backgroundImage: NetworkImage(
+                        "https://i.pinimg.com/736x/de/f1/f3/def1f3321c7340363d5e60657d4aef59.jpg",
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Mir Riazul Islam",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "mirirazul859@gmail.com",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(dense: true, title: Text("HOME"), onTap: () {}),
+              Divider(color: Colors.blueGrey, thickness: 1),
+              ListTile(
+                visualDensity: (VisualDensity.compact),
+                title: Text("PROFILE"),
+                onTap: () {},
+              ),
+              Divider(color: Colors.blueGrey, thickness: 1),
+              ListTile(
+                visualDensity: VisualDensity.comfortable,
+                title: Text("SETTINGS"),
+                onTap: () {},
+              ),
+              Divider(color: Colors.blueGrey, thickness: 1),
+              ListTile(
+                visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                title: Text("LOGOUT"),
+                onTap: () {},
+              ),
+              Divider(color: Colors.blueGrey, thickness: 1),
+            ],
+          ),
+        ),
+
+        body: TabBarView(
+          children: [
+            // Container(
+            //   height: 20,
+            //   color: Colors.brown,
+            //   child: Center(
+            //     child: Text(
+            //       'Home',
+            //       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            //     ),
+            //   ),
+            // ),
+            // Container(
+            //   height: 20,
+            //   color: Colors.lightBlue,
+            //   child: Center(
+            //     child: Text(
+            //       'Settings',
+            //       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            //     ),
+            //   ),
+            // ),
+            // Container(
+            //   height: 20,
+            //   color: Colors.green,
+            //   child: Center(
+            //     child: Text(
+            //       'Favorites',
+            //       style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            //     ),
+            //   ),
+            // ),
+            Alert(),
+            module8_stack(),
+            module8class3(),
           ],
         ),
       ),
