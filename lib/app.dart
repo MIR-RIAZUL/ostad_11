19028a5bccea6f89c01dfddb2a8b010eb8b7300c
MIR-RIAZUL <mirriazul859@gmail.com>
2025-08-10 14:04:11 +0600

    calculator ui button create

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:first/module_8/class1.dart';
 import 'package:flutter/material.dart';
 
 import 'module_11/class_1/counter_app.dart';
+import 'module_11/class_2/calculator.dart';
 import 'module_9/class_2_module_9.dart';
 
 class MyApp extends StatelessWidget {
@@ -13,12 +14,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'flutter batch 11',
-      initialRoute: '/counterApp',
+      initialRoute: '/Calculator',
       routes: {
         '/counterApp': (context) => counterApp(),
         '/second': (context) => class2_m9(),
         '/third': (context) => class3(),
         '/fourth': (context) => class1(),
+        '/Calculator': (context) => Calculator(),
       },
       // home: class3m9(),
     );
--- lib/module_11/class_2/calculator.dart
+++ lib/module_11/class_2/calculator.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+
+                onPressed: () {},
+                child: Text('1'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
